c4d168aac01f10ff85b38ff426794ca78242d945
Olaitan Awe <laitanawe@gmail.com>
2020-03-11 14:47:03 -0400

    update parsing script to parse every in helix logs

--- parse_logs.py
+++ parse_logs.py
@@ -1,15 +1,17 @@
 #!/bin/python
 
 import io, glob, os
-for file in os.listdir("helixlogs/"):
-#for file in glob.glob("*.txt"):
-with io.open("file","r") as f:
+path = 'helixlogs/'
+for mylogfile in glob.glob(os.path.join(path, '*')):
+    print("\n", mylogfile)
+    with open(mylogfile, "r") as f:
+        text = f.read()
+        c = f.read()
+        lines = text.split("\n")
+        newlist = [ l.split() for l in lines ]
+        yetanothernewlist = [ mylist for mylist in newlist if mylist ]
+        items = [data[1:] for data in yetanothernewlist]
+        record_list = [ { val.split("=")[0]:val.split("=")[1] for val in sublist } for sublist in items]
+        print(record_list)
 
-c = f.read()
-lines = c.split("\n")
-
-newlist = [ l.split() for i in lines ]
-yetanothernewlist = [ mylist for mylist in newlist if mylist ]
-items = [data[1:] for data in yetanothernewlist]
-record_list = [ { val.split("=")[0]:val.split("=")[1] for val in sublist } for sublist in items]
-print(record_list)
+#with io.open("file","w") as output:
