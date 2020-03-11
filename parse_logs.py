57930af88e04055ed8c0aa447452708a1970ab6b
Olaitan Awe <laitanawe@gmail.com>
2020-03-11 15:49:34 -0400

    updated parsing script

--- parse_logs.py
+++ parse_logs.py
@@ -3,7 +3,7 @@
 This script parses every log file in the Helix Log Directory and prints the corresponding JSON
 '''
 import io, glob, os
-path = 'helixlogs/'
+path = 'helix_logs/'
 for mylogfile in glob.glob(os.path.join(path, '*')):
     print("\n")
     print(mylogfile)
@@ -13,7 +13,12 @@ for mylogfile in glob.glob(os.path.join(path, '*')):
         newlist = [ l.split() for l in lines ]
         yetanothernewlist = [ mylist for mylist in newlist if mylist ]
         items = [data[1:] for data in yetanothernewlist]
-        record_list = [ { val.split("=")[0]:val.split("=")[1] for val in sublist } for sublist in items]
-        print(record_list)
+        try:
+            record_list = [ { val.split("=")[0]:val.split("=")[1] for val in sublist } for sublist in items]
+        except ValueError:
+            print(f"Cannot split record(s) in log file: {mylogfile}")
+        else:
+            print("Success")
+            print(record_list)
 
 #with io.open("file","w") as output:
